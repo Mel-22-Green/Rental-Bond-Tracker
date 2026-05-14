cb667d8fee1c50b810cd2f013a97a4e4ce357c87
K240050-asim <k240050@student.kent.edu.au>
2026-05-14 22:51:28 +1000

    Update Documents.js

--- frontend/pages/Documents.js
+++ frontend/pages/Documents.js
@@ -1,5 +1,5 @@
 ﻿import React, { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';  
 import API from '../services/api';
 
 function Documents() {
